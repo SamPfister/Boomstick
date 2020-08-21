1afe018ea03559652adaaf1271ff553981f89b22
SamPfister <54378666+SamPfister@users.noreply.github.com>
2020-08-21 14:29:22 -0500

    First commit
    There's a soccer field, and the player can move around and jump

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //Credit to Brackeys fo rhte code: https://www.youtube.com/watch?v=_QajrabyTJc&t=115s
+
+    public float gravity = -10f;
+
+    public float speed = 12f;
+
+    public float jumpHeight = 3f;
+
+    public CharacterController controller;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+ 
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(-2f * gravity * jumpHeight);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
