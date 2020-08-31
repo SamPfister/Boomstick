1916da588b5cfff2abcd2140be65aad786344e20
BottomKek <darkspineemerald1234@gmail.com>
2020-08-31 08:02:26 -0500

    bruh

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
-    public float range = 100f;
+    public float range = 10f;
     public Camera fpsCam;
+<<<<<<< HEAD
+    public float gunForce = 2000f;
+=======
     public float gunForce = 1000f;
+    public ParticleSystem muzzleFlash;
+>>>>>>> cefc114d0f61cec02cb6ec7b456dd6d172d91797
 
     void Update()
     {
@@ -18,6 +23,7 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
