2e88795af3237e02c7ac36aea382d63033116070
SamPfister <54378666+SamPfister@users.noreply.github.com>
2020-09-02 07:57:26 -0500

    Revert "bruh"
    This reverts commit 1916da588b5cfff2abcd2140be65aad786344e20.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
-    public float range = 10f;
+    public float range = 100f;
     public Camera fpsCam;
-<<<<<<< HEAD
-    public float gunForce = 2000f;
-=======
     public float gunForce = 1000f;
-    public ParticleSystem muzzleFlash;
->>>>>>> cefc114d0f61cec02cb6ec7b456dd6d172d91797
 
     void Update()
     {
@@ -23,7 +18,6 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
-        muzzleFlash.Play();
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
