7c40524f93852962305c5dd399ee941c6958669f
SamPfister <54378666+SamPfister@users.noreply.github.com>
2020-09-02 07:55:10 -0500

    Revert "maybe 2"
    This reverts commit 70763b877096553577c54bdbfd70d6dfec1251c0.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,11 +6,12 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 10f;
     public Camera fpsCam;
-
+<<<<<<< HEAD
     public float gunForce = 2000f;
-
+=======
+    public float gunForce = 1000f;
     public ParticleSystem muzzleFlash;
-
+>>>>>>> cefc114d0f61cec02cb6ec7b456dd6d172d91797
 
     void Update()
     {
